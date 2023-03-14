11ef9ae831a643b525f6ab2a7273e14c98cadc71
Winai Nadee <winain@gmail.com>
2023-03-14 17:04:30 +0700

    update deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -1,16 +1,18 @@
 #!/usr/bin/env sh
 
 # abort on errors
-#set -e
+# set -e
 
 # build
 yarn build
 
 # navigate into the build output directory
-cd .vuepress/dist
+# cd .vuepress/dist
+Set-Location .vuepress/dist
 
 # if you are deploying to a custom domain
-echo 'mentor2code.com' > CNAME
+# echo 'mentor2code.com' > CNAME
+Write-Output 'mentor2code.com' > CNAME
 
 git init
 git add -A
@@ -22,4 +24,5 @@ git push -f git@github.com:mentor2code/mentor2code.github.io.git master
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
-cd -
+# cd -
+Set-Location ../..
